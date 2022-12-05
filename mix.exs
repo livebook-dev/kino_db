dfec8cec60ab5de79bbd98c0c8832026f4a40a12
Jonatan Kłosko <jonatanklosko@gmail.com>
2022-12-05 21:14:44 +0100

    Release 0.2.1

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule KinoDB.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @description "Databases integration with Livebook"
 
   def project do
@@ -26,7 +26,7 @@ defmodule KinoDB.MixProject do
 
   defp deps do
     [
-      {:kino, "~> 0.6.1 or ~> 0.7.0"},
+      {:kino, "~> 0.7"},
       {:table, "~> 0.1.2"},
       {:postgrex, "~> 0.16.3 or ~> 0.17", optional: true},
       {:exqlite, "~> 0.11", optional: true},
