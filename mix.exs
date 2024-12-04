9ed00606b3e3002d8a2dd94ff3112239ce3fea57
Jonatan Kłosko <jonatanklosko@gmail.com>
2024-12-04 15:45:00 +0800

    Bump req_athena

--- mix.exs
+++ mix.exs
@@ -37,8 +37,7 @@ defmodule KinoDB.MixProject do
       {:explorer, "~> 0.10", optional: true},
 
       # Those dependecies are new, so we use stricter versions
-      # TODO bump to 0.3.0
-      {:req_athena, "~> 0.2.0", optional: true},
+      {:req_athena, "~> 0.3.0", optional: true},
       {:req_ch, "~> 0.1.0", optional: true},
 
       # Dev only
